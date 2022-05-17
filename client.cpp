eb49567d800d7da17c3afd1b121929ee657861c8
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2022-05-17 17:45:18 +0200

    Sprawdzanie, czy podano wymagane argumenty

--- client.cpp
+++ client.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
 #include <boost/program_options.hpp>
 #include <boost/asio.hpp>
 
 namespace p_opt = boost::program_options;
 
 int main(int argc, char *argv[]) {
+    std::string player_name;
+    std::string gui_address;
+    std::string server_address;
+    uint16_t port;
+
     p_opt::options_description description("Allowed options");
     description.add_options()
-            ("gui-address,d", "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>")
+            ("gui-address,d", p_opt::value<std::string>(&gui_address), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>")
             ("help,h", "Wypisuje jak używać programu")
-            ("player-name,n", "Nazwa gracza")
-            ("port,p", "Port na którym klient nasłuchuje komunikatów od GUI")
-            ("server-address,s", "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>")
+            ("player-name,n", p_opt::value<std::string>(&player_name), "Nazwa gracza")
+            ("port,p", p_opt::value<uint16_t>(&port), "Port na którym klient nasłuchuje komunikatów od GUI")
+            ("server-address,s", p_opt::value<std::string>(&server_address), "<(nazwa hosta):(port) lub (IPv4):(port) lub (IPv6):(port)>")
     ;
 
     p_opt::variables_map var_map;
@@ -22,6 +28,31 @@ int main(int argc, char *argv[]) {
         std::cout << description << "\n";
         return 1;
     }
+
+    if (var_map.count("gui-address") == 0) {
+        std::cout << "No gui address\n";
+        return 1;
+    }
+    std::cout << "gui address: " << gui_address << "\n";
+
+    if (var_map.count("port") == 0) {
+        std::cout << "No gui port\n";
+        return 1;
+    }
+    std::cout << "port: " << port << "\n";
+
+    if (var_map.count("server-address") == 0) {
+        std::cout << "No server address\n";
+        return 1;
+    }
+    std::cout << "server address: " << server_address << "\n";
+
+
+    if (var_map.count("player-name") == 0) {
+        std::cout << "No player name\n";
+        return 1;
+    }
+    std::cout << "player-name: " << player_name << "\n";
 
     return 0;
 }
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ asio_test: asio_test.cpp
 	g++ -Wall -Wextra -Wconversion -Werror -O2 -std=gnu++20 -fanalyzer -o asio_test.o asio_test.cpp
 
 client: client.cpp
-	g++ -Wall -Wextra -Wconversion -Werror -O2 -std=gnu++20 -fanalyzer -o client.o client.cpp -lboost_program_options
+	g++ -O2 -Wextra -Wconversion -Werror -std=gnu++20 -o client.o client.cpp -lboost_program_options -pthread
 
 clean:
 	rm *.o
