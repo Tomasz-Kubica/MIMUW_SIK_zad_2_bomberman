5b13efb4cfab733efe59873d36a4ab3322ae7f16
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2022-05-18 18:01:43 +0200

    Parsowanie map list i par przy pomocy conceptów

--- common.h
+++ common.h
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <string>
 #include <unordered_map>
+#include <concepts>
 
 /* = = = *
  * TYPES *
@@ -24,7 +25,7 @@ template<>
 std::optional<uint8_t> parse<uint8_t>(char* *buffer, size_t *bytes_to_read) {
     if (*bytes_to_read < 1)
         return {};
-    uint8_t result = (uint8_t)(**buffer);
+    auto result = *(uint8_t*)*buffer;
     *buffer += 1;
     *bytes_to_read -= 1;
     return result;
@@ -34,7 +35,7 @@ template<>
 std::optional<uint16_t> parse<uint16_t>(char* *buffer, size_t *bytes_to_read) {
     if (*bytes_to_read < 2)
         return {};
-    uint8_t result = (uint16_t)(**buffer);
+    auto result = *(uint16_t*)*buffer;
     *buffer += 2;
     *bytes_to_read -= 2;
     return ntohs(result);
@@ -44,7 +45,7 @@ template<>
 std::optional<uint32_t> parse<uint32_t>(char* *buffer, size_t *bytes_to_read) {
     if (*bytes_to_read < 4)
         return {};
-    uint8_t result = (uint32_t)(**buffer);
+    auto result = *(uint32_t*)*buffer;
     *buffer += 4;
     *bytes_to_read -= 4;
     return ntohl(result);
@@ -65,37 +66,87 @@ std::optional<std::string> parse(char* *buffer, size_t *bytes_to_read) {
 }
 
 template<typename T>
-std::optional<std::vector<T>> parse_vec(char* *buffer, size_t *bytes_to_read) {
+concept List = requires(T t, typename T::value_type e, size_t i) {
+    typename T::value_type;
+    t.push_back(e);
+    { t.at(i) } -> std::convertible_to<typename T::value_type>;
+    { t.size() } -> std::same_as<size_t>;
+};
+
+template<List T>
+std::optional<T> parse(char* *buffer, size_t *bytes_to_read) {
     auto size = parse<uint32_t>(buffer, bytes_to_read);
     if (!size)
         return {};
     auto size_value = size.value();
-    std::vector<T> result;
+    T result;
     for (uint32_t i = 0; i < size_value; i++) {
-        auto to_push_option = parse<T>(buffer, bytes_to_read);
+        auto to_push_option = parse<typename T::value_type>(buffer, bytes_to_read);
         if (!to_push_option)
             return {};
         result.push_back(to_push_option.value());
     }
+    return result;
 }
 
-template<typename K, typename V>
-std::optional<std::unordered_map<K, V>> parse_map(char* *buffer, size_t *bytes_to_read) {
+template<typename T>
+concept Map = requires(
+    T t,
+    typename T::value_type e,
+    typename T::mapped_type v,
+    typename T::key_type k
+) {
+    typename T::value_type;
+    typename T::mapped_type;
+    typename T::key_type;
+    typename T::iterator;
+    t.insert(e);
+    { t.size() } -> std::convertible_to<size_t>;
+    { t.begin() } -> std::same_as<typename T::iterator>;
+    { t.end() } -> std::same_as<typename T::iterator>;
+    requires std::same_as<typename T::value_type, std::pair<const typename T::key_type, typename T::mapped_type>>;
+};
+
+template<Map T>
+std::optional<T> parse(char* *buffer, size_t *bytes_to_read) {
     auto size = parse<uint32_t>(buffer, bytes_to_read);
     if (!size)
         return {};
     auto size_value = size.value();
-    std::unordered_map<K, V> result;
+    T result;
     for (uint32_t i = 0; i < size_value; i++) {
-        auto key_option = parse<K>(buffer, bytes_to_read);
+        auto key_option = parse<typename T::key_type>(buffer, bytes_to_read);
         if (!key_option)
             return {};
-        auto value_option = parse<V>(buffer, bytes_to_read);
+        auto value_option = parse<typename T::mapped_type>(buffer, bytes_to_read);
         if (!value_option)
             return {};
         result.insert({key_option.value(), value_option.value()});
     }
     return result;
+}
+
+template<typename T>
+concept Pair = requires(T t) {
+    typename T::first_type;
+    typename T::second_type;
+    t.first;
+    t.second;
+    requires std::same_as<decltype(t.first), typename T::first_type>;
+    requires std::same_as<decltype(t.second), typename T::second_type>;
+};
+
+template<Pair T>
+std::optional<T> parse(char* *buffer, size_t *bytes_to_read) {
+    auto first = parse<typename T::first_type>(buffer, bytes_to_read);
+    auto second = parse<typename T::second_type>(buffer, bytes_to_read);
+    if (first && second) {
+        T result;
+        result.first = first.value();
+        result.second = second.value();
+        return result;
+    }
+    return {};
 }
 
 /* = = = = = *
